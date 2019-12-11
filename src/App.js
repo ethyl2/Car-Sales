fd7a2eb6eb107515672aa73b1bb1e2dccd8f40f6
Heather Nuffer <zzzuzz@gmail.com>
2019-12-11 13:57:09 -0700

    Connected Header to the store

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = () => {
+  /*
   const state = {
     additionalPrice: 0,
     car: {
@@ -22,6 +23,7 @@ const App = () => {
       { id: 4, name: 'Rear spoiler', price: 250 }
     ]
   };
+  */
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
